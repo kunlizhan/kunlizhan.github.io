39e65a7120716c87589bc7f630054092f2321633
kunlizhan <36314560+kunlizhan@users.noreply.github.com>
2020-07-08 02:19:51 -0400

    test json for first post

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function makeBtnBg() {
 makeBtnBg();
 
 function makeReadMore() {
-  $("#main > div").append('<div class="readMore"><div>Full Article</div></div>');
+  $("#main > div").append(
+    '<div class="readMore"><div><i class="fa fa-file-text" aria-hidden="true"></i> Full article &emsp14; <i class="fa fa-comments-o" aria-hidden="true"></i> Comments</div></div>'
+  );
 }
 makeReadMore();
 
@@ -20,3 +22,10 @@ $('.btn').hover(
     $( this ).children(".btn-bg-light").stop(true,false).fadeTo(300,0);
   }
 );
+
+function loadHomeFeed() {
+  $.getJSON("post/list.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+  });
+}
+loadHomeFeed();
